4f334de2964c43df2fbcc44ac4c5405194a9960a
CJ Gebo <46651000+cjg2901@users.noreply.github.com>
2022-02-22 20:11:14 -0500

    Update information.bat

--- information.bat
+++ information.bat
@@ -5,36 +5,63 @@ exit /b
 
 :sub
 ::basic inventory
+echo Hostname:
 hostname
+echo:
+echo Ipconfig:
 ipconfig /all
+echo:
+echo Operating System:
 systeminfo | findstr OS
 
 ::check for listening ports
+echo:
+echo Listening Ports:
 netstat -ano | findstr LIST | findstr /V ::1 | findstr /V 127.0.0.1
 
 ::users and groups
+echo:
+echo Users:
 net user
+echo:
+echo Groups:
 net localgroup
-net localgroup “Administrators”
-net localgroup “Remote Desktop Users”
+echo:
+echo Administrators Users:
+net localgroup "Administrators"
+echo:
+echo Remote Desktop Users:
+net localgroup "Remote Desktop Users"
 
 ::looking for network shares
+echo:
+echo Network Shares:
 net share
 
 ::Check startup programs
+echo:
+echo Startup Programs:
 reg query HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run
 reg query HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce
 reg query HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
 reg query HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce
 
 ::services
+echo:
+echo Services:
 net start
 
 ::processes
+echo:
+echo Processes:
 tasklist /svc /FO table
 
 ::exe files
+echo:
+echo Exe files:
 dir /B /S \*.exe
 
 ::system32
+echo:
+echo system32:
 dir /B /S \windows\system32
