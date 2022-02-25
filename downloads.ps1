1a8fe8b5fae1941a8a42c3a32e62da7ebe11e8e3
CJ Gebo <46651000+cjg2901@users.noreply.github.com>
2022-02-25 17:40:47 -0500

    Create downloads.ps1

---
+++ downloads.ps1
@@ -0,0 +1,8 @@
+(New-Object System.Net.WebClient).DownloadFile(“https://download.sysinternals.com/files/SysinternalsSuite.zip”, “C:\sysinternals.zip”)
+Expand-Archive -LiteralPath C:\sysinternals.zip -DestinationPath C:\sysinternals
+
+(New-Object System.Net.WebClient).DownloadFile(“https://explorerplusplus.com/software/explorer++_1.3.5_x64.zip”, “C:\explorerplusplus.zip”)
+Expand-Archive -LiteralPath C:\explorerplusplus.zip -DestinationPath C:\explorerplusplus
+
+(New-Object System.Net.WebClient).DownloadFile(“https://www.google.com/url?q=https://2.na.dl.wireshark.org/win64/Wireshark-win64-3.6.2.zip”, “C:\wireshark.zip”)
+Expand-Archive -LiteralPath C:\wireshark.zip -DestinationPath C:\wireshark
