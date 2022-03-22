2816ffeefe6c2137b2d9451b76d81037aa4bef30
Kayla Hodgson <60293418+noheartxo@users.noreply.github.com>
2022-03-22 11:18:47 -0400

    Update downloads.ps1
    Changing .zip back to .exe for Wireshark download as .zip does not work.

--- downloads.ps1
+++ downloads.ps1
@@ -4,5 +4,5 @@ Expand-Archive -LiteralPath C:\sysinternals.zip -DestinationPath C:\sysinternals
 (New-Object System.Net.WebClient).DownloadFile(“https://explorerplusplus.com/software/explorer++_1.3.5_x64.zip”, “C:\explorerplusplus.zip”)
 Expand-Archive -LiteralPath C:\explorerplusplus.zip -DestinationPath C:\explorerplusplus
 
-(New-Object System.Net.WebClient).DownloadFile(“https://www.google.com/url?q=https://2.na.dl.wireshark.org/win64/Wireshark-win64-3.6.2.zip”, “C:\wireshark.zip”)
+(New-Object System.Net.WebClient).DownloadFile(“https://2.na.dl.wireshark.org/win64/Wireshark-win64-3.6.2.exe”, “C:\wireshark.exe”)
 Expand-Archive -LiteralPath C:\wireshark.zip -DestinationPath C:\wireshark
