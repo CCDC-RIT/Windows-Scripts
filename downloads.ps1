13819ec786de3ffea2f8844f70536fc65f53f530
Chandi Kanhai <chandikanhai@gmail.com>
2023-01-10 20:48:47 -0500

    changes to downloads and folder structure

--- downloads.ps1
+++ downloads.ps1
@@ -6,59 +6,84 @@
 # Load zip assembly: [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
 # Unzip file: [System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, $targetDir)
 
-# Deciding what user management tools to download
-mkdir scripts
+$ErrorActionPreference = "Stop"
+
+[ValidateScript({
+    if(-not (Test-Path -Path $_ -PathType Container))
+    {
+        throw "Invalid Path"
+    }
+    $true
+})]
+$UserPath = Read-Host -Prompt "Input absolute path to download files"
+Set-Location -Path $UserPath
+
+$ErrorActionPreference = "Continue"
+New-Item -Path $UserPath -Name "scripts" -ItemType "directory"
+New-Item -Path $UserPath -Name "setup" -ItemType "directory"
+New-Item -Path $UserPath -Name "tools" -ItemType "directory"
+$ScriptPath = Join-Path -Path $UserPath -ChildPath "scripts"
+$SetupPath = Join-Path -Path $UserPath -ChildPath "setup"
+$ToolsPath = Join-Path -Path $UserPath -ChildPath "tools"
+
+# Certain tools are specific to types of systems
 $ad = Read-Host "Is this a Domain Controller (Y or N)? "
-if ($ad -eq "N") {
+if (($ad -eq "N") -or ($ad -eq "n")) {
     Write-Host "Downloading local user management script..."
-    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/blum.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\blum.bat")
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/blum.bat", (Join-Path -Path $ScriptPath -ChildPath "blum.bat"))
 } 
 $core = Read-Host "Is this server a Core Server (Y or N)? "
-if ($core -eq "Y") {
-    (New-Object System.Net.WebClient).DownloadFile("https://github.com/derceg/explorerplusplus/releases/download/version-1.4.0-beta-2/explorerpp_x64.zip", "C:\Program Files\WindowsPowershell\pickles\epp.zip")
-    Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\epp.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\epp"
+if (($core -eq "Y") -or ($ad -eq "y")) {
+    (New-Object System.Net.WebClient).DownloadFile("https://github.com/derceg/explorerplusplus/releases/download/version-1.4.0-beta-2/explorerpp_x64.zip", (Join-Path -Path $UserPath -ChildPath "epp.zip"))
+    Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "epp.zip") -DestinationPath (Join-Path -Path $ToolsPath -ChildPath "epp")
 }
 
-# Other scripts/files
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/audit.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\audit.bat")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/firewall.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\firewall.bat")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/inventory.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\inventory.bat")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/logging.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\logging.bat")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/netstat.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\netstat.bat")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/secure.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\secure.bat")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/secpol.inf", "C:\Program Files\WindowsPowershell\pickles\scripts\secpol.inf")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/folderintegrity.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\folderintegrity.bat")
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/goldentickets.bat", "C:\Program Files\WindowsPowershell\pickles\scripts\goldentickets.bat")
+# Scripts and config files 
+New-Item -Path $ScriptPath -Name "conf" -ItemType "directory"
+New-Item -Path $ScriptPath -Name "results" -ItemType "directory"
+$ConfPath = Join-Path -Path $ScriptPath -ChildPath "conf"
 
-# Sysinternals
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Autoruns.zip", "C:\Program Files\WindowsPowershell\pickles\ar.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/TCPView.zip", "C:\Program Files\WindowsPowershell\pickles\tv.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Sysmon.zip", "C:\Program Files\WindowsPowershell\pickles\sm.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessExplorer.zip", "C:\Program Files\WindowsPowershell\pickles\pe.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessMonitor.zip", "C:\Program Files\WindowsPowershell\pickles\pm.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Sigcheck.zip", "C:\Program Files\WindowsPowershell\pickles\sc.zip")
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ListDlls.zip", "C:\Program Files\WindowsPowershell\pickles\dll.zip")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/audit.bat", (Join-Path -Path $ScriptPath -ChildPath "audit.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/firewall.bat", (Join-Path -Path $ScriptPath -ChildPath "firewall.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/inventory.bat", (Join-Path -Path $ScriptPath -ChildPath "inventory.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/logging.bat", (Join-Path -Path $ScriptPath -ChildPath "logging.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/netstat.bat", (Join-Path -Path $ScriptPath -ChildPath "netstat.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/secure.bat", (Join-Path -Path $ScriptPath -ChildPath "secure.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/secpol.inf", (Join-Path -Path $ScriptPath -ChildPath "secpol.inf"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/folderintegrity.bat", (Join-Path -Path $ScriptPath -ChildPath "folderintegrity.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/goldentickets.bat", (Join-Path -Path $ScriptPath -ChildPath "goldentickets.bat"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/log_generator.bat", (Join-Path -Path $ScriptPath -ChildPath "log_generator.bat"))
+# Get-InjectedThread
+(New-Object System.Net.WebClient).DownloadFile("https://gist.githubusercontent.com/jaredcatkinson/23905d34537ce4b5b1818c3e6405c1d2/raw/104f630cc1dda91d4cb81cf32ef0d67ccd3e0735/Get-InjectedThread.ps1", (Join-Path -Path $ScriptPath -ChildPath "Get-InjectedThread.ps1"))
+# Basic Sysmon config file
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml", (Join-Path -Path $ConfPath -ChildPath "sysmon.xml"))
+# TODO: wazuh config file
 
+# Installers programs for various tools
 # Windows Firewall Control
-(New-Object System.Net.WebClient).DownloadFile("https://www.binisoft.org/download/wfc6setup.exe", "C:\Program Files\WindowsPowershell\pickles\wfwcsetup.exe")
-
+(New-Object System.Net.WebClient).DownloadFile("https://www.binisoft.org/download/wfc6setup.exe", (Join-Path -Path $SetupPath -ChildPath "wfcsetup.exe"))
 # Wireshark
 # (for now) TLS 1.2 link: https://wireshark.marwan.ma/download/win64/Wireshark-win64-latest.exe
-(New-Object System.Net.WebClient).DownloadFile("https://1.na.dl.wireshark.org/win64/Wireshark-win64-latest.exe", "C:\Program Files\WindowsPowershell\pickles\wsinstall.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://1.na.dl.wireshark.org/win64/Wireshark-win64-latest.exe", (Join-Path -Path $SetupPath -ChildPath "wsinstall.exe"))
+# Wazuh agent
+(New-Object System.Net.WebClient).DownloadFile("https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi", (Join-Path -Path $SetupPath -ChildPath "wazuhagent.msi"))
 
-# Basic Sysmon config file
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml", "C:\Program Files\WindowsPowershell\pickles\sysmonconfig.xml")
-
-# Get-InjectedThread
-(New-Object System.Net.WebClient).DownloadFile("https://gist.githubusercontent.com/jaredcatkinson/23905d34537ce4b5b1818c3e6405c1d2/raw/104f630cc1dda91d4cb81cf32ef0d67ccd3e0735/Get-InjectedThread.ps1", "C:\Program Files\WindowsPowershell\pickles\Get-InjectedThread.ps1")
+# Sysinternals
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Autoruns.zip", (Join-Path -Path $UserPath -ChildPath "ar.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ListDlls.zip", (Join-Path -Path $UserPath -ChildPath "dll.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessExplorer.zip", (Join-Path -Path $UserPath -ChildPath "pe.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessMonitor.zip", (Join-Path -Path $UserPath -ChildPath "pm.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Sigcheck.zip", (Join-Path -Path $UserPath -ChildPath "sc.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Sysmon.zip", (Join-Path -Path $UserPath -ChildPath "sm.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/TCPView.zip", (Join-Path -Path $UserPath -ChildPath "tv.zip"))
 
 # Unzipping stuff
-mkdir sys
-Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\ar.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\sys\ar"
-Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\tv.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\sys\tv"
-Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\sm.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\sys\sm"
-Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\pe.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\sys\pe"
-Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\pm.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\sys\pm"
-Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\sc.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\sys\sc"
-Expand-Archive -LiteralPath "C:\Program Files\WindowsPowershell\pickles\dll.zip" -DestinationPath "C:\Program Files\WindowsPowershell\pickles\sys\dll"
-
+New-Item -Path $ToolsPath -Name "sys" -ItemType "directory"
+$SysPath = Join-Path -Path $ToolsPath -ChildPath "sys"
+Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "ar.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "ar")
+Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "dll.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "dll")
+Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "pe.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "pe")
+Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "pm.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "pm")
+Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "sc.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "sc")
+Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "sm.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "sm")
+Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "tv.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "tv")
