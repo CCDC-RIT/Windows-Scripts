7a014883af4b5de1bee06781c8da0c58ab9ff974
Chandi Kanhai <chandikanhai@gmail.com>
2023-10-05 19:54:03 -0400

    downloads script mostly made

--- wc/downloads.ps1
+++ wc/downloads.ps1
@@ -5,74 +5,85 @@
 # Unzip file: [System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, $targetDir)
 
 $ErrorActionPreference = "Stop"
-
 [ValidateScript({
     if(-not (Test-Path -Path $_ -PathType Container))
     {
-        throw "Invalid Path"
+        Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "ERROR" -ForegroundColor red -NoNewLine; Write-Host "] Invalid path" -ForegroundColor white
+	    break
     }
     $true
 })]
-$UserPath = Read-Host -Prompt "Input absolute path to download files:"
-Set-Location -Path $UserPath
+$InputPath = Read-Host -Prompt "Input absolute path to download files"
+Set-Location -Path $InputPath
 
+# Creating all the directories
 $ErrorActionPreference = "Continue"
-New-Item -Path $UserPath -Name "scripts" -ItemType "directory"
-New-Item -Path $UserPath -Name "setup" -ItemType "directory"
-New-Item -Path $UserPath -Name "tools" -ItemType "directory"
-$ScriptPath = Join-Path -Path $UserPath -ChildPath "scripts"
-$ToolsPath = Join-Path -Path $UserPath -ChildPath "tools"
+New-Item -Path $InputPath -Name "scripts" -ItemType "directory" | Out-Null
+New-Item -Path $InputPath -Name "installers" -ItemType "directory" | Out-Null
+New-Item -Path $InputPath -Name "tools" -ItemType "directory" | Out-Null
+$ScriptPath = Join-Path -Path $InputPath -ChildPath "scripts"
+$SetupPath = Join-Path -Path $InputPath -ChildPath "installers"
+$ToolsPath = Join-Path -Path $InputPath -ChildPath "tools"
 
-# Certain tools are specific to types of systems
-if (!(Get-WmiObject -Query 'select * from Win32_OperatingSystem where (ProductType = "2")')) {
-    Write-Host "[INFO] Downloading local user management script..."
-    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/blum.bat", (Join-Path -Path $ScriptPath -ChildPath "blum.bat"))
-    # LGPO
-    (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip", (Join-Path -Path $ToolsPath -ChildPath "LGPO.zip"))
-    Expand-Archive -LiteralPath (Join-Path -Path $ToolsPath -ChildPath "LGPO.zip") -DestinationPath (Join-Path -Path $ToolsPath -ChildPath "LGPO")
-} 
-
-# Scripts and config files 
-New-Item -Path $ScriptPath -Name "conf" -ItemType "directory"
-New-Item -Path $ScriptPath -Name "results" -ItemType "directory"
+New-Item -Path $ScriptPath -Name "conf" -ItemType "directory" | Out-Null
+New-Item -Path $ScriptPath -Name "results" -ItemType "directory" | Out-Null
 $ConfPath = Join-Path -Path $ScriptPath -ChildPath "conf"
+$ResultsPath = Join-Path -Path $ScriptPath -ChildPath "results"
 
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/audit.bat", (Join-Path -Path $ScriptPath -ChildPath "audit.bat"))
+New-Item -Path $ResultsPath -Name "artifacts" -ItemType "directory" | Out-Null
+New-Item -Path $ToolsPath -Name "sys" -ItemType "directory" | Out-Null
+$SysPath = Join-Path -Path $ToolsPath -ChildPath "sys"
+
+Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] Directories created" -ForegroundColor white
+
+# DC detection
+if (Get-CimInstance -Class Win32_OperatingSystem -Filter 'ProductType = "2"') {
+    Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "INFO" -ForegroundColor yellow -NoNewLine; Write-Host "] Domain Controller detected" -ForegroundColor white
+    # TODO: links to downloading the DC-specific GPO and security template
+} else {
+    (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip", (Join-Path -Path $InputPath -ChildPath "lg.zip"))
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/member-client/wc-member-client-v6.inf", (Join-Path -Path $ConfPath -ChildPath "wc-secpol.inf"))
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/member-client/%7B4BB1406C-78CC-44D0-B229-A1B9F6753187%7D.zip", (Join-Path -Path $ConfPath -ChildPath "{4BB1406C-78CC-44D0-B229-A1B9F6753187}.zip"))
+    Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] Non-DC GPO, security template, and LGPO downloaded" -ForegroundColor white
+
+    Expand-Archive -LiteralPath (Join-Path -Path $InputPath -ChildPath "lg.zip") -DestinationPath $ToolsPath
+    Expand-Archive -LiteralPath (Join-Path -Path $ConfPath -ChildPath "{4BB1406C-78CC-44D0-B229-A1B9F6753187}.zip") -DestinationPath (Join-Path -Path $ConfPath -ChildPath "{4BB1406C-78CC-44D0-B229-A1B9F6753187}")
+    Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] LGPO and non-DC GPO extracted" -ForegroundColor white
+}
+
+
+# Scripts
+# (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/audit.ps1", (Join-Path -Path $ScriptPath -ChildPath "audit.ps1"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/bum.ps1", (Join-Path -Path $ScriptPath -ChildPath "bum.ps1"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/inventory.ps1", (Join-Path -Path $ScriptPath -ChildPath "inventory.ps1"))
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/firewall.ps1", (Join-Path -Path $ScriptPath -ChildPath "firewall.ps1"))
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/logging.ps1", (Join-Path -Path $ScriptPath -ChildPath "logging.ps1"))
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/sbaseline.ps1", (Join-Path -Path $ScriptPath -ChildPath "sbaseline.ps1"))
-# Get-InjectedThread
 (New-Object System.Net.WebClient).DownloadFile("https://gist.githubusercontent.com/jaredcatkinson/23905d34537ce4b5b1818c3e6405c1d2/raw/104f630cc1dda91d4cb81cf32ef0d67ccd3e0735/Get-InjectedThread.ps1", (Join-Path -Path $ScriptPath -ChildPath "Get-InjectedThread.ps1"))
-# Hidden services
-(New-Object System.Net.WebClient).DownloadFile("https://gist.githubusercontent.com/joswr1ght/c5d9773a90a22478309e9e427073fd30/raw/128fd9bde4ef989c7037e1b5c51d14ace823146f/checkhiddensvc.ps1", (Join-Path -Path $ScriptPath -ChildPath "checkhiddensvc.ps1"))
-# Security policy file
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/wc-client-member-v5.inf", (Join-Path -Path $ConfPath -ChildPath "secpol.inf"))
-# Audit policy file
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/wc-auditpol-v1.csv", (Join-Path -Path $ConfPath -ChildPath "auditpol.csv"))
-# GPO
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/CCDC-RIT/Windows-Scripts/raw/master/wc/%7BA1E02EB6-57AA-428A-A17C-2182E09F31F8%7D.zip", (Join-Path -Path $ConfPath -ChildPath "{A1E02EB6-57AA-428A-A17C-2182E09F31F8}.zip"))
-Expand-Archive -LiteralPath (Join-Path -Path $ConfPath -ChildPath "{A1E02EB6-57AA-428A-A17C-2182E09F31F8}.zip") -DestinationPath (Join-Path -Path $ConfPath -ChildPath "{A1E02EB6-57AA-428A-A17C-2182E09F31F8}")
+Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] Scripts downloaded" -ForegroundColor white
 
-# Installers for various tools
-# Malwarebytes
-(New-Object System.Net.WebClient).DownloadFile("https://www.malwarebytes.com/api/downloads/mb-windows?filename=MBSetup.exe", (Join-Path -Path $ToolsPath -ChildPath "MBSetup.exe"))
-# PatchMyPC
+# Config files
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/wc-auditpol-v1.csv", (Join-Path -Path $ConfPath -ChildPath "wc-auditpol.csv"))
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/defender-exploit-guard-settings.xml", (Join-Path -Path $ConfPath -ChildPath "def-eg-settings.xml"))
+Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] Audit policy and Defender Exploit Guard settings downloaded" -ForegroundColor white
+
+# Installers for various tools + Sysinternals
+(New-Object System.Net.WebClient).DownloadFile("https://www.malwarebytes.com/api/downloads/mb-windows?filename=MBSetup.exe", (Join-Path -Path $SetupPath -ChildPath "MBSetup.exe"))
 (New-Object System.Net.WebClient).DownloadFile("https://patchmypc.com/freeupdater/PatchMyPC.exe", (Join-Path -Path $ToolsPath -ChildPath "PatchMyPC.exe"))
+Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] MalwareBytes setup and PatchMyPC downloaded" -ForegroundColor white
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Autoruns.zip", (Join-Path -Path $InputPath -ChildPath "ar.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ListDlls.zip", (Join-Path -Path $InputPath -ChildPath "dll.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessExplorer.zip", (Join-Path -Path $InputPath -ChildPath "pe.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessMonitor.zip", (Join-Path -Path $InputPath -ChildPath "pm.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Sigcheck.zip", (Join-Path -Path $InputPath -ChildPath "sc.zip"))
+(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/TCPView.zip", (Join-Path -Path $InputPath -ChildPath "tv.zip"))
+Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] SysInternals tools downloaded" -ForegroundColor white
 
-# Sysinternals
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Autoruns.zip", (Join-Path -Path $UserPath -ChildPath "ar.zip"))
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ListDlls.zip", (Join-Path -Path $UserPath -ChildPath "dll.zip"))
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessExplorer.zip", (Join-Path -Path $UserPath -ChildPath "pe.zip"))
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessMonitor.zip", (Join-Path -Path $UserPath -ChildPath "pm.zip"))
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/Sigcheck.zip", (Join-Path -Path $UserPath -ChildPath "sc.zip"))
-(New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/TCPView.zip", (Join-Path -Path $UserPath -ChildPath "tv.zip"))
-
-# Unzipping stuff
-New-Item -Path $ToolsPath -Name "sys" -ItemType "directory"
-$SysPath = Join-Path -Path $ToolsPath -ChildPath "sys"
-Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "ar.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "ar")
-Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "dll.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "dll")
-Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "pe.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "pe")
-Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "pm.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "pm")
-Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "sc.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "sc")
-Expand-Archive -LiteralPath (Join-Path -Path $UserPath -ChildPath "tv.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "tv")
+# Unzip mode
+Expand-Archive -LiteralPath (Join-Path -Path $InputPath -ChildPath "ar.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "ar")
+Expand-Archive -LiteralPath (Join-Path -Path $InputPath -ChildPath "dll.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "dll")
+Expand-Archive -LiteralPath (Join-Path -Path $InputPath -ChildPath "pe.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "pe")
+Expand-Archive -LiteralPath (Join-Path -Path $InputPath -ChildPath "pm.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "pm")
+Expand-Archive -LiteralPath (Join-Path -Path $InputPath -ChildPath "sc.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "sc")
+Expand-Archive -LiteralPath (Join-Path -Path $InputPath -ChildPath "tv.zip") -DestinationPath (Join-Path -Path $SysPath -ChildPath "tv")
+Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] SysInternals tools extracted" -ForegroundColor white
