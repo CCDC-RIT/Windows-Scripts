46dad663ce08d242f3df1211c9ce0691414c9bf0
Chandi Kanhai <chandikanhai@gmail.com>
2023-10-14 00:47:32 -0400

    downloads script fix

--- wc/downloads.ps1
+++ wc/downloads.ps1
@@ -41,10 +41,10 @@ if (Get-CimInstance -Class Win32_OperatingSystem -Filter 'ProductType = "2"') {
     Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "INFO" -ForegroundColor yellow -NoNewLine; Write-Host "] Domain Controller detected" -ForegroundColor white
     # TODO: links to downloading the DC-specific GPO and security template
     (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/dc/wc-dc-v1.inf", (Join-Path -Path $ConfPath -ChildPath "wc-dc-secpol.inf"))
-    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/member-client/%7BC697CBFC-C192-45CF-8873-6BD96F5A8AE1%7D.zip", (Join-Path -Path $ConfPath -ChildPath "{C697CBFC-C192-45CF-8873-6BD96F5A8AE1}.zip"))
+    (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/CCDC-RIT/Windows-Scripts/master/wc/dc/%7BC697CBFC-C192-45CF-8873-6BD96F5A8AE1%7D.zip", (Join-Path -Path $ConfPath -ChildPath "{C697CBFC-C192-45CF-8873-6BD96F5A8AE1}.zip"))
     Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] DC GPO and security template downloaded" -ForegroundColor white
 
-    Expand-Archive -LiteralPath (Join-Path -Path $ConfPath -ChildPath "{C697CBFC-C192-45CF-8873-6BD96F5A8AE1}") -DestinationPath (Join-Path -Path $ConfPath -ChildPath "{C697CBFC-C192-45CF-8873-6BD96F5A8AE1}")
+    Expand-Archive -LiteralPath (Join-Path -Path $ConfPath -ChildPath "{C697CBFC-C192-45CF-8873-6BD96F5A8AE1}.zip") -DestinationPath (Join-Path -Path $ConfPath -ChildPath "{C697CBFC-C192-45CF-8873-6BD96F5A8AE1}")
     Write-Host "[" -ForegroundColor white -NoNewLine; Write-Host "SUCCESS" -ForegroundColor green -NoNewLine; Write-Host "] DC GPO extracted" -ForegroundColor white
 } else {
     (New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip", (Join-Path -Path $InputPath -ChildPath "lg.zip"))
