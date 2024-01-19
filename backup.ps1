bae3dea80785a63be91c67b6fcc29a0b8a585635
James Southcott <James.southcott19@gmail.com>
2024-01-18 20:16:23 -0500

    changed paths so backups save in our directory

--- backup.ps1
+++ backup.ps1
@@ -1,22 +1,24 @@
-if (Get-CimInstance -Class Win32_OperatingSystem -Filter 'ProductType = "2"') {
-    # TODO: edit path to be proper folder
-    robocopy c:\windows\sysvol c:\sysvol-backup /copyall /mir /b /r:0 /xd
+[string]$path = ($MyInvocation.MyCommand.Path).substring(0,($MyInvocation.MyCommand.Path).indexOf("scripts\backup.ps1"))
+New-Item -Path $path -Name "backup" -ItemType "directory" | Out-Null
+[string]$backupPath = (Join-Path $path "backup")
 
-    $backup = Join-Path -Path (Get-Item -Path '..').FullName -ChildPath "\dns_backup"
-    xcopy /E /I C:\Windows\System32\dns $backup
+if (Get-CimInstance -Class Win32_OperatingSystem -Filter 'ProductType = "2"') {
+    New-Item -Path $backupPath -Name "sysvol" -ItemType "directory" | Out-Null
+    robocopy c:\windows\sysvol (Join-Path $backupPath "sysvol") /copyall /mir /b /r:0 /xd
+
+    New-Item -Path $backupPath -Name "dns_backup" -ItemType "directory" | Out-Null
+    xcopy /E /I C:\Windows\System32\dns (Join-Path -Path $backupPath -childPath "dsn_backup")
 }
 
 
 # Wazuh agent backup 
 $dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
-$bkpFolder = Join-Path -Path (Get-Item -Path '..').FullName -ChildPath "\wazuh_files_backup\$dateTime"
-New-Item -ItemType Directory -Path $bkpFolder | Out-Null
 
-xcopy "C:\Program Files (x86)\ossec-agent\client.keys" $bkpFolder /H /I /K /S /X
-xcopy "C:\Program Files (x86)\ossec-agent\ossec.conf" $bkpFolder /H /I /K /S /X
-xcopy "C:\Program Files (x86)\ossec-agent\internal_options.conf" $bkpFolder /H /I /K /S /X
-xcopy "C:\Program Files (x86)\ossec-agent\local_internal_options.conf" $bkpFolder /H /I /K /S /X
-xcopy "C:\Program Files (x86)\ossec-agent\*.pem" $bkpFolder /H /I /K /S /X
-xcopy "C:\Program Files (x86)\ossec-agent\ossec.log" $bkpFolder /H /I /K /S /X
-xcopy "C:\Program Files (x86)\ossec-agent\logs\*"  $bkpFolder\logs\ /H /I /K /S /X
-xcopy "C:\Program Files (x86)\ossec-agent\rids\*"  $bkpFolder\rids\ /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\client.keys" $backupPath /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\ossec.conf" $backupPath /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\internal_options.conf" $backupPath /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\local_internal_options.conf" $backupPath /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\*.pem" $backupPath /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\ossec.log" $backupPath /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\logs\*"  $backupPath\logs\ /H /I /K /S /X
+xcopy "C:\Program Files (x86)\ossec-agent\rids\*"  $backupPath\rids\ /H /I /K /S /X
