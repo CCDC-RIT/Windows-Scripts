3ae50b88547f9d15ccedbdb4924667acf6ef4376
Chandi Kanhai <chandikanhai@gmail.com>
2024-04-06 20:34:17 -0400

    backup script fixes

--- backup.ps1
+++ backup.ps1
@@ -6,13 +6,14 @@ if (Get-CimInstance -Class Win32_OperatingSystem -Filter 'ProductType = "2"') {
     New-Item -Path $backupPath -Name "sysvol" -ItemType "directory" | Out-Null
     robocopy c:\windows\sysvol (Join-Path $backupPath "sysvol") /copyall /mir /b /r:0 /xd
 
+    dnscmd /exportsettings | Out-Null
     New-Item -Path $backupPath -Name "dns_backup" -ItemType "directory" | Out-Null
-    xcopy /E /I C:\Windows\System32\dns (Join-Path -Path $backupPath -childPath "dsn_backup")
+    xcopy /E /I C:\Windows\System32\dns (Join-Path -Path $backupPath -childPath "dns_backup")
 }
 
 
 # Wazuh agent backup 
-$dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+# $dateTime = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 
 xcopy "C:\Program Files (x86)\ossec-agent\client.keys" $backupPath /H /I /K /S /X
 xcopy "C:\Program Files (x86)\ossec-agent\ossec.conf" $backupPath /H /I /K /S /X
