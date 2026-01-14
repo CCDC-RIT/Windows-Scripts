f000f3cbc990e519021db31803f369a038df7466
Ricker Wilkes <107262607+R-Wilkes@users.noreply.github.com>
2026-01-13 19:14:12 -0500

    Changed color to Magenta

--- profile.ps1
+++ profile.ps1
@@ -4,4 +4,4 @@ Set-PsReadLineOption -EditMode Windows
 # Any other settings that are needed to secure the current powershell session on boot
 
 # Little message upon profile load
-Write-Host "CCDC PROFILE LOADED" -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host "CCDC PROFILE LOADED" -ForegroundColor Magenta -BackgroundColor White
